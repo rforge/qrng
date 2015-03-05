904a653b721a270fb80beecbeedbdb7444a61acc
mhofert <mhofert@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2015-03-05 16:08:13 +0000

    rm foo
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/qrng@10 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/foo.R
+++
@@ -1,5 +0,0 @@
-myRfun <- function(arg1, arg2)
-{
-    stopifnot(arg1 > 0, arg2 > 0)
-    .Call(myCfun, arg1, arg2)
-}
--- pkg/R/qmc.R
+++ pkg/R/qmc.R
@@ -1,2 +1,47 @@
-###
+### Main interface to (randomized) quasi-random number generators
+
+
+
+rKorobov <- function(n, d, method=c("R", "C")) {
+
+}
+
+
+## qrn <- function(n, d, quasi.method=c("MC", "Korobov", "Korobov.shift",
+##                 "Korobov.primitve"), random.method=c("default", "shift.mod.1",
+##                                      "digital.shift", "scramble"), n.random, method=c("C", "R")...)
+## {
+##     ## input checking
+##     quasi.method <- match.arg(quasi.method)
+##     switch(quasi.method,
+##            "Korobov" = {
+##                ## call C code
+##                if(hasArg(a/z))
+##                    choose good default 'a' if not specified (depend on 'n')
+
+##                else {
+##                    Korobov.type....
+##                }
+##                hasArg(a or vector of z's)
+
+
+##            },
+##            "" = {
+
+##            },
+##            stop("wrong quasi.method"))
+
+
+## ## randomization
+## ...
+
+##     n <- param_n
+##     d <- s
+##     korobov.generator <- param_a
+
+
+
+
+
+## }
 
