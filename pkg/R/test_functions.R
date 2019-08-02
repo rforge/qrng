4f823a372fb614739a3eeedaf2c23124331b863b
mhofert <mhofert@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2019-08-02 04:28:54 +0000

    improvements in test functions
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/qrng@64 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/test_functions.R
+++ pkg/R/test_functions.R
@@ -35,43 +35,54 @@ sobol_g <- function(u, copula = indepCopula(dim = ncol(u)), alpha = 1:ncol(u), .
 
 ##' @title Computing Indicators of Rows Exceeding a Threshold
 ##' @param x (n, d)-matrix containing the n d-dimensional realizations
-##' @param p d-vector of probabilities determining the (joint) threshold
-##'        through componentwise quantiles
+##' @param q exceedance threshold; can be a d-vector for method = "indicator"
+##' @param p number or vector (for method = "indicator") or number
+##'        (for method = "individual.given.sum.exceeds" or
+##'        method = "sum.given.sum.exceeds") defining the
+##'        probability/ies whose empirical quantile is/are used as threshold if
+##'        q is not given.
 ##' @param method character string indicating the type of exceedance computed
 ##' @return Exceedance indicators whether X > x
 ##' @author Marius Hofert
-##' @note - Run time can be improved for large d by checking the first dimension
-##'         that falls below p.
-##'       - If you want an estimator of P(X > x) = E(<test function>),
-##'         call mean(exceedance())
-exceedance <- function(x, p = 0.99, method = c("indicator",
-                                               "individual.given.sum.exceeds",
-                                               "sum.given.sum.exceeds"))
+##' @note Run time can be improved for large d by checking the first dimension
+##'       that falls below q.
+exceedance <- function(x, q, p = 0.99, method = c("indicator",
+                                                  "individual.given.sum.exceeds",
+                                                  "sum.given.sum.exceeds"))
 {
+    stopifnot(0 <= p, p <= 1)
     if(!is.matrix(x)) x <- rbind(x)
     d <- ncol(x)
     method <- match.arg(method)
     switch(method,
            "indicator" = {
-               q <- if(length(p) == 1) {
-                        apply(x, 2, quantile, probs = p, names = FALSE, type = 1)
-                    } else {
-                        stopifnot(length(p) == d)
-                        sapply(seq_len(d), function(j)
-                            quantile(x[,j], probs = p[j], names = FALSE, type = 1))
-                    }
+               if(!hasArg(q)) { # ... determine q empirically from p
+                   q <- if(length(p) == 1) {
+                            apply(x, 2, quantile, probs = p, names = FALSE, type = 1)
+                        } else {
+                            stopifnot(length(p) == d)
+                            sapply(seq_len(d), function(j)
+                                quantile(x[,j], probs = p[j], names = FALSE, type = 1))
+                        }
+               } else {
+                   if(length(q) == 1) q <- rep(q, d)
+               }
                rowSums(x > rep(q, each = nrow(x))) == d
            },
            "individual.given.sum.exceeds" = {
                stopifnot(length(p) == 1)
                s <- rowSums(x)
-               q <- quantile(s, probs = p, names = FALSE, type = 1)
+               if(!hasArg(q)) {
+                   q <- quantile(s, probs = p, names = FALSE, type = 1)
+               } else stopifnot(length(q) == 1)
                x[s > q,]
            },
            "sum.given.sum.exceeds" = {
                stopifnot(length(p) == 1)
                s <- rowSums(x)
-               q <- quantile(s, probs = p, names = FALSE, type = 1)
-               s[s > q,]
+               if(!hasArg(q)) {
+                   q <- quantile(s, probs = p, names = FALSE, type = 1)
+               } else stopifnot(length(q) == 1)
+               s[s > q]
            }, stop("Wrong 'method'"))
 }
